defc97a7dabfd7999a577e7cdad6b5f4a606389b
root <1445829583@qq.com>
2020-03-12 01:30:42 +0800

    first commit

---
+++ changgou/changgou_parent/changgou_service/changgou_service_goods/src/main/java/com/changgou/service/goods/dao/BrandMapper.java
@@ -0,0 +1,9 @@
+package com.changgou.service.goods.dao;
+
+import com.changgou.goods.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+
+public interface BrandMapper extends Mapper<Brand> {
+
+}
